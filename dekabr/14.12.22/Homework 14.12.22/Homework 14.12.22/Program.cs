b55aeba40b0b3d1a77fd914790012405f47af11e
EhmedMustafa <prince.1987@mail.ru>
2022-12-18 17:05:29 +0400

    14.12.22

---
+++ dekabr/14.12.22/Homework 14.12.22/Homework 14.12.22/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_14._12._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Homework-1
+            /*
+            İstifadəçidən 2 dənə ayrı dəyər alın(rəqəm). Məsələn birincidə istifadəçi 20 daxil etdi onu a dəyişənində saxlayın.
+            İkincidə 50 daxil etdi onu da b dəyişənində saxlayın.Elə edin ki, bu iki dəyişənin dəyərlərinin yerlərini dəyişdirin.
+            Yəni a 20, b 50 - dirsə elə edin ki, a olsun 50, b olsun 20.*/
+
+            //int varint = Convert.ToInt32(Console.ReadLine());
+            //int varint1 = Convert.ToInt32(Console.ReadLine());
+            //var B = varint;
+            //var C = varint1;
+            //B = varint1;
+            //C = varint;
+            #endregion
+
+            #region Homework-2
+            /* Homework 2: İstifadəçidən 3 ayrı sətirdə 3 ayrı rəqəm alın.İstifadəçi 3 - cü rəqəmi daxil etdikdən sonra ekrana belə bir yazı
+                         yazdırın Nəticə: 5 + 5 + 10 = 20.Bunu çıxma, vurma və bölmə üçün də edin.*/
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int c = Convert.ToInt32(Console.ReadLine());
+
+            //int netice = a + b + c;
+            //Console.WriteLine("Netice:" + a +"+" + b +"+"+c+"+" +"=" + netice);
+
+            #endregion
+
+            #region HOmework-3
+            /*
+            Homework 3: İstifadəçidən 10 dənə fərqli rəqəm alıb bir dəyişəndə toplayın.Bu taskı da çıxma, vurma və bölmə üçün də edin.
+                         Çıxmada və toplama əməliyyatlarında 10 - cu dəyər daxil edildikdən sonra alınan nəticəyə 1 toplayacaq və ya çıxacaqsız.
+                         Vurmada bölmədə isə, alınan nəticəni 20'ə vurun və ya bölün.*/
+
+            //int v = Convert.ToInt32(Console.ReadLine());
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //int d = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int w = Convert.ToInt32(Console.ReadLine());
+            //int r = Convert.ToInt32(Console.ReadLine());
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //int j = Convert.ToInt32(Console.ReadLine());
+            //int netice = v + a + c + d + b + n + w + r + y + j;
+            //int cem = 1;
+            //int g= netice - cem;
+
+            //Console.WriteLine("cavab:"+ g);
+
+
+
+            #endregion
+
+            #region homwork-4
+            /* Homework 4: İstifadəçidən qeyd etdiyim tiplərdə dəyər alıb onları uyğun tipdə dəyişənlərə çevirəcəksiz.
+                          bool, string, short, long, int, char, datetime.*/
+
+            bool varbool = Convert.ToBoolean(Console.ReadLine());
+            bool boolen = varbool;
+            string varsting = Convert.ToString(Console.ReadLine());
+            string varstirn = varsting;
+            short varshort = Convert.ToInt16(Console.ReadLine());
+            short varshort1 = varshort;
+            long varlong = Convert.ToInt64(Console.ReadLine());
+            long varlong1 = varlong;
+            int varint = Convert.ToInt32(Console.ReadLine());
+            int varint2 = varint;
+            char varchar = Convert.ToChar(Console.ReadLine());
+            char varchar2 = varchar;
+            DateTime newdatetime = Convert.ToDateTime(Console.ReadLine());
+            DateTime newdt = newdatetime;
+
+
+
+
+
+
+
+
+
+            #endregion
+        }
+    }
+}
