7cb34034bbf7811b7732dbe9dac7bfa934d4c5da
Ehmedmustafa <prince.1987@mail.ru>
2023-01-10 22:39:42 +0400

    25.12.22

---
+++ dekabr/18.12.22/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Homework 1:
+             Əvvəlcədən iki dəyişəndə username və password saxlayın.Bunlar heç bir zaman dəyişməyəcək şəkildə olsun.Yəni proqramda belə dəyişdirmək mümkün olmasın
+             Sonra istifadəçidən username və password'u daxil etməsini istəyin. Əgər istifadəçi doğru məlumatları yazsa ekrana "Sisteme daxil olundu." mesajı çıxsın.
+             Yox əgər səhv məlumat daxil etsə, yenidən username və password'u daxil etməsini istəyin. 3 dəfə səhv daxil etdikdən sonra "Hesab bloklandi" yazsın.
+             Not: Switch case və döngülərdən istifadə etmək olmaz.*/
+
+            string username = "Ehmed";
+            string password = "1987";
+            username = Console.ReadLine();
+            password = Console.ReadLine();
+            if (username=="ehmed"&&password=="1987")
+            {
+                Console.WriteLine("daxil olduz");
+                Console.Read();
+            }
+            else
+            {
+                username = Console.ReadLine();
+                password = Console.ReadLine();
+                if (username == "ehmed" && password == "1987")
+                {
+                    Console.WriteLine("daxil olduz");
+                }
+                else
+                {
+                    username = Console.ReadLine();
+                    password = Console.ReadLine();
+                    if (username == "ehmed" && password == "1987")
+                    {
+                        Console.WriteLine("daxil olduz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("BLOK");
+                    }
+
+                }
+                
+            }
+         
+
+        }
+
+
+    }
+}
---
+++ dekabr/25.12.22/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task-1
+            //Homework 1: İstifadəçi mənfi dəyər daxil edənə qədər, daxil etdiyi bütün müsbət rəqəmləri toplayıb axırda nəticəni ekrana yazın.
+            int reqem;
+            int a = 0;
+            Console.WriteLine("Zehmet olmasa reqem daxil edin:");
+            while (true)
+            {
+                reqem = int.Parse(Console.ReadLine());
+                a += reqem; ;
+                if (reqem < 0)
+                {
+                    Console.WriteLine(a); break;
+                }
+            }
+            #endregion
+
+            #region task-4
+            //Homework 4: İstifadəçidən bir rəqəm alın. While döngüsüylə istifadəçinin daxil etdiyi rəqəmdən 0-a doğru geriyə sayın.
+            int reqem1;
+            reqem1= Convert.ToInt32(Console.ReadLine());
+            while (reqem1>= 0)
+            {
+                Console.WriteLine(reqem1);
+                reqem--;
+            }
+            #endregion
+
+            #region task-3
+            /*omework 3: App'ın işləmə prinsipi: İlk öncə istifadəçidən "Texmin oyunu ucun en yuksek deyeri daxil edin: " yazısı ilə bir rəqəm
+              alınmalıdır.Sonra 1 ilə bu rəqəm arasında random bir rəqəm generate edilməlidir və istifadəçidən generate olunan rəqəmi
+                           tapmağı istənməlidir. "1. Cəhdiniz! Zəhmət olmasa rəqəmi təxmin edin: " mesajı ilə istifadəçidən bir rəqəm soruşulur. 
+                           Hər səhv daxil edilən rəqəmdə eyni şey təkrar göstərilir.Sadəcə 2. 3. 4.deyə artır cəhd sayı.İstifadəçi düzgün rəqəmi
+                        tapdığı zaman "5. Cəhdinizdə düzgün rəqəmi tapdınız!" mesajı göstərilməlidir.*/
+
+            Console.WriteLine("Texmin oyunu ucun en yuksek deyeri daxil edin: ");
+            int say = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+
+
+                Console.WriteLine("Cehdiniz! Zehmet olmasa reqemi texmin edin: ");
+                int j = Convert.ToInt32(Console.ReadLine());
+                Random rand = new Random();
+                int b = rand.Next(1, say);
+                Console.WriteLine(b);
+
+                if (j == b)
+                {
+                    Console.WriteLine("Cehdinizde düzgün reqemi tapdınız!"); break;
+                }
+                #endregion
+            }
+
+        }
+    }
+}
